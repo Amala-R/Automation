a207752ceab4dd2480bc782ca3dbe2a4283aa951
Amala R <129738213+Amala-R@users.noreply.github.com>
2023-04-10 14:38:30 +0530

    Removing modify user references

--- src/test/java/DeleteUserTestCase.java
+++ src/test/java/DeleteUserTestCase.java
@@ -1,28 +1,19 @@
 import framework.common.Assertions;
-import framework.common.RestUtil;
 import framework.exceptions.AutomationException;
 import framework.propertiesManagement.TestProperties;
 import framework.services.UserProfileService;
-import org.json.JSONObject;
 import org.testng.annotations.Test;
-
-import static framework.globalConstants.JsonConstants.RESPONSE_STRING_NAME;
 
 public class DeleteUserTestCase {
 
     @Test
     public static void deleteUserProfile() throws AutomationException {
-        RestUtil modifyUser =
-                UserProfileService
-                        .init()
-                        .getUserProfileForUpdating(TestProperties.init().getProperty("user.name"));
-        JSONObject jsonObject = new JSONObject(modifyUser);
-        JSONObject newJsonToUpdate = new JSONObject(jsonObject.get(RESPONSE_STRING_NAME).toString());
+        String getUserToDelete = TestProperties.init().getProperty("user.name");
         UserProfileService
                 .init()
-                .deleteUserProfiles(TestProperties.init().getProperty("user.name"));
+                .deleteUserProfiles(getUserToDelete);
 
-        Assertions.shouldBeTrue("\nDeleted JSON is : "+newJsonToUpdate);
+        Assertions.shouldBeTrue("\nDeleted user is : "+getUserToDelete);
         Assertions.SOFT_ASSERT.assertAll();
     }
 }
